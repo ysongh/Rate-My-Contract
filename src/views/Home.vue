bfe1095ee7364f3e7807b2755994f12646863b20
You Song Hou <yousonghou@gmail.com>
2022-03-22 00:25:34 -0400

    feat: add a card template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,73 @@
 <template>
-  <div class="home">
+  <v-container>
     <h1>Rate My Contract</h1>
-  </div>
+
+    <v-card
+      class="mb-3"
+    >
+      <v-card-text>
+        <h1>
+          Title
+        </h1>
+        <br />
+        <div class="text--primary">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non officiis et minima quae enim quisquam eligendi velit ea, sequi voluptatibus. Officia consectetur deleniti vero magnam deserunt facilis quo, id doloribus?
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          text
+          color="teal accent-4"
+        >
+          Learn More
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-card
+      class="mb-3"
+    >
+      <v-card-text>
+        <h1>
+          Title
+        </h1>
+        <br />
+        <div class="text--primary">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non officiis et minima quae enim quisquam eligendi velit ea, sequi voluptatibus. Officia consectetur deleniti vero magnam deserunt facilis quo, id doloribus?
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          text
+          color="teal accent-4"
+        >
+          Learn More
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-card
+      class="mb-3"
+    >
+      <v-card-text>
+        <h1>
+          Title
+        </h1>
+        <br />
+        <div class="text--primary">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non officiis et minima quae enim quisquam eligendi velit ea, sequi voluptatibus. Officia consectetur deleniti vero magnam deserunt facilis quo, id doloribus?
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          text
+          color="teal accent-4"
+        >
+          Learn More
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
