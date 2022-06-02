415edf689b5a8529014f3bef3695f6cad61ccc99
You Song Hou <yousonghou@gmail.com>
2022-06-01 22:44:57 -0400

    feat: install '@uauth/js' and add login with Unstoppable Domain

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,13 +10,46 @@
     </router-link>
 
     <v-spacer></v-spacer>
-    
+    <v-btn
+      v-if="!domainName"
+      color="green"
+      @click="loginWithUnstoppableDomains()"
+    >
+      Login With Unstoppable Domain
+    </v-btn>
+    <p>{{domainName}}</p>
   </v-app-bar>
 </template>
 
 <script>
-export default {
+import UAuth from '@uauth/js'
 
+import { UNSTOPPABLEDOMAINS_CLIENTID, UNSTOPPABLEDOMAINS_REDIRECT_URI } from '../config'
+
+const uauth = new UAuth({
+  clientID: UNSTOPPABLEDOMAINS_CLIENTID,
+  redirectUri: UNSTOPPABLEDOMAINS_REDIRECT_URI,
+  scope: "openid wallet email:optional humanity_check:optional"
+});
+
+export default {
+  name: "Navbar",
+  data: () => ({
+    domainName: ''
+  }),
+  methods: {
+    async loginWithUnstoppableDomains() {
+      try {
+        const authorization = await uauth.loginWithPopup()
+        authorization.sub = authorization.idToken.sub
+        console.log(authorization)
+
+        this.domainName = authorization.idToken.sub
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
 }
 </script>
 
